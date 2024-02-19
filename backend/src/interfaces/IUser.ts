2da4f7f128b2ee298bdcbeb02491ba207560e34f
Willian Batista <willian.alves.b15@gmail.com>
2024-02-19 15:19:41 -0300

    created UserRepository

---
+++ backend/src/interfaces/IUser.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  created_at?: Date;
+}
+
+export interface IUserModel {
+  findUser(email: string): Promise<IUser | null>;
+  createUser(data: IUser): Promise<IUser | null>;
+}
---
+++ backend/src/repositories/user.repository.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import { IUser, IUserModel } from '../interfaces/IUser';
+
+const prisma = new PrismaClient();
+
+export default class UserRepository implements IUserModel {
+  
+  async findUser(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+    return user
+  }
+
+  async createUser(data: IUser) {
+    const createdUser = await prisma.user.create({ data });
+    return createdUser
+  }
+}
