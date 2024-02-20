7618b6d65d2b81c34167927216a103bd215dad2e
Willian Batista <willian.alves.b15@gmail.com>
2024-02-20 20:35:47 -0300

    feat: validate Token

--- backend/src/routes/barber.routes.ts
+++ backend/src/routes/barber.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { BarberFactory } from "../factory";
+import validateJWT from "../middleware/validateJWT";
 
 const barberRouter = Router();
 
-barberRouter.post('/barber', (req, res, next) => {
+barberRouter.post('/barber', validateJWT, (req, res, next) => {
   BarberFactory().createBarber(req, res, next);
 })
 
--- backend/src/utils/generateJWT.ts
+++ backend/src/utils/generateJWT.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import * as jwt from "jsonwebtoken";
 import { IUserToken } from "../interfaces/IUser";
 
-const SECRET = "jwt_secret";
-const algorithm = "HS256";
+export const SECRET = "jwt_secret";
+export const algorithm = "HS256";
 
 function generateJWT(payload: Omit<IUserToken, "password">) {
   const secret = process.env.JWT_SECRET || SECRET;
