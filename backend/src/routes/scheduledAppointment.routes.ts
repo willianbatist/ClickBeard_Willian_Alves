fc362a4f6d4ccf9fb70672b78394dac0aa32712e
Willian Batista <willian.alves.b15@gmail.com>
2024-02-21 10:38:08 -0300

    feat: create validateCreateScheduledAppointment

--- backend/src/routes/scheduledAppointment.routes.ts
+++ backend/src/routes/scheduledAppointment.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { ScheduledAppointmentFactory } from "../factory";
 import validateJWT from "../middleware/validateJWT";
+import { validateCreateScheduledAppointment } from "../middleware/scheduledAppointment.middleware";
 
 const ScheduledAppointmentRouter = Router();
 
-ScheduledAppointmentRouter.post("/scheduledAppointment", validateJWT, (req, res, next) => {
+ScheduledAppointmentRouter.post("/scheduledAppointment", validateJWT, validateCreateScheduledAppointment, (req, res, next) => {
   ScheduledAppointmentFactory().create(req, res, next)
 })
 
