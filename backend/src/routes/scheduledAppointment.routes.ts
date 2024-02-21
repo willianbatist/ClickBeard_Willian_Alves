33dc87acbc355b4296becf0947b3f0888be4b4d4
Willian Batista <willian.alves.b15@gmail.com>
2024-02-21 10:30:17 -0300

    feat: add validateJWT in ScheduledAppointmentRouter

--- backend/src/routes/scheduledAppointment.routes.ts
+++ backend/src/routes/scheduledAppointment.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { ScheduledAppointmentFactory } from "../factory";
+import validateJWT from "../middleware/validateJWT";
 
 const ScheduledAppointmentRouter = Router();
 
-ScheduledAppointmentRouter.post("/scheduledAppointment", (req, res, next) => {
+ScheduledAppointmentRouter.post("/scheduledAppointment", validateJWT, (req, res, next) => {
   ScheduledAppointmentFactory().create(req, res, next)
 })
 
